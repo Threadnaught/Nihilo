e489c276e8db83ea1103127398d3f9806282294b
idk <patrickhingley@live.co.uk>
2020-10-08 00:42:50 +0100

    added aspirational TODOs to manifest.json

--- scripts/allfiles.sh
+++ scripts/allfiles.sh
@@ -3,3 +3,4 @@
 find . -not -path ./common_deps/\* | grep \\.[ch] > allfiles.txt
 find . -not -path ./common_deps/\* | grep \\.yaml >> allfiles.txt
 find . -not -path ./common_deps/\* | grep Dockerfile >> allfiles.txt
+find . -not -path ./common_deps/\* | grep \\.json >> allfiles.txt
