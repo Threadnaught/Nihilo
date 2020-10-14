faaeb816b906703c9218908abaf9c4b6f1bc71c4
idk <patrickhingley@live.co.uk>
2020-10-14 22:50:32 +0100

    small demo

--- machine_prototypes/default/main.c
+++ machine_prototypes/default/main.c
@@ -6,5 +6,8 @@
 #include "../../include/api.h"
 
 void entry(char* arg){
-	printf("hello\n");
+	queue("nihilo_host~195734F260E4E4782B432F9A71A6EB69EA4EE821D9D72FDC4C68F227F3D54D1C", "test", NULL, NULL, NULL, 0);
+}
+void test(char* arg){
+	printf("hello, there!\n");
 }
