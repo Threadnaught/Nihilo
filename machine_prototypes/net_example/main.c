cf96f2ec0ee7d6777622296d7c560e1d088ea213
idk <patrickhingley@live.co.uk>
2020-10-28 23:42:34 +0000

    Housekeeping

--- machine_prototypes/net_example/main.c
+++ machine_prototypes/net_example/main.c
@@ -8,11 +8,10 @@
 //same as com_example, but across a network
 
 void entry(const char* arg){
-	/*char* server_address = read_DB("server_address", NULL);
+	char* server_address = read_DB("server_address", NULL);
 	printf("querying place out of %s\n", server_address);
 	queue(server_address, "get_place", "success", "failure", NULL, 0);
-	free(server_address);*/
-	queue_str("138.68.154.50~3FD13DBF06D063A8A299B3B6DD2510E48D496FE031BFDF3B4ED147FC1D8309AB", "update_page", NULL, NULL, "bah");
+	free(server_address);
 }
 void get_place(const char* arg){
 	uint32_t ret_len;
